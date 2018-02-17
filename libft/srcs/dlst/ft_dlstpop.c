e85029859173cb00c35231add73f3ad0c174e588
Vincent LAY <vlay@e1r5p14.42.fr>
2018-02-17 16:02:21 +0100

    Derpy hooves

--- libft/srcs/dlst/ft_dlstpop.c
+++ libft/srcs/dlst/ft_dlstpop.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstpop.c                                        :+:      :+:    :+:   */
+/*   ft_dlstpop.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 18:27:57 by mdeville          #+#    #+#             */
-/*   Updated: 2018/02/15 19:34:49 by mdeville         ###   ########.fr       */
+/*   Updated: 2018/02/17 15:32:19 by vlay             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,14 @@
 
 t_dlist	*ft_dlstpop(t_dlist **alst)
 {
+	void	*content;
 	t_dlist	*res;
 
 	if (!alst || !*alst)
 		return (NULL);
-	if ((*alst)->prev)
-		(*alst)->prev->next = (*alst)->next;
 	res = *alst;
 	*alst = (*alst)->next;
-	res->next = NULL;
-	res->prev = NULL;
-	return (res);
+	content = res->content;
+	free(res);
+	return (content);
 }
